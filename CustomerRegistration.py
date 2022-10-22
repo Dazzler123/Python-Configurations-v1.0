5aa3af63c28f2a69390e8b013a42ef6e0dd32745
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-22 16:34:22 +0530

    Input textfields added

--- CustomerRegistration.py
+++ CustomerRegistration.py
@@ -12,4 +12,15 @@ Label(root, text="Customer Name").grid(row=2, column=2)
 Label(root, text="Customer Address").grid(row=3, column=2)
 Label(root, text="Customer Salary").grid(row=4, column=2)
 
+cusID = StringVar
+cusName = StringVar
+cusAddress = StringVar
+cusSalary = DoubleVar
+
+# input textfields
+Entry(root, textvariable=cusID).grid(row=1, column=3)
+Entry(root, textvariable=cusName).grid(row=2, column=3)
+Entry(root, textvariable=cusAddress).grid(row=3, column=3)
+Entry(root, textvariable=cusSalary).grid(row=4, column=3)
+
 root.mainloop()
