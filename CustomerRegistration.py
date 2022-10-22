bc226f9ab5f383be496a576b4831c629efa2c9aa
Dazzler123 <dasinduhewagamage@gmail.com>
2022-10-22 16:33:11 +0530

    Content added

---
+++ CustomerRegistration.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+root.geometry("500x300")
+
+# header label
+Label(root, text="Registration Form", font="ar 15 bold").grid(row=0, column=3)
+
+
+root.mainloop()
